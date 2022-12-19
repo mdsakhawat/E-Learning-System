83fdb2cc804c37b6d0d05ce6269f58ac95ec2792
Siam <smsiam40@gmail.com>
2022-12-19 22:50:23 +0600

    Removed cached

--- routes/user/searchpage.route.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const controller = require('../../controller/search-controller');
-router.post('/category', controller.searchbody);
-router.get('/category', controller.searchvalue);
-
-module.exports = router;
